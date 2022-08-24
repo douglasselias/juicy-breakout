305cff8978eef03f6809410b36d53e7653369938
Douglas S. Elias <douglassantoselias@gmail.com>
2022-08-24 16:08:33 -0300

    add bouncing ball

--- main.cpp
+++ main.cpp
@@ -15,16 +15,39 @@ int main(void)
   player.x = GetScreenWidth() / 2 - player.width / 2;
   player.y = GetScreenHeight() - player.height;
 
+  int ballRadius = 10;
+  int ballRadii = ballRadius / 2;
+  Vector2 ballPosition = {};
+  ballPosition.x = GetScreenWidth() / 2;
+  ballPosition.y = GetScreenHeight() / 2;
+  Vector2 ballSpeed = {};
+  ballSpeed.x = 300;
+  ballSpeed.y = 300;
+
   while (!WindowShouldClose())
   {
-    float mousePosition = GetMouseX() - player.width / 2;
-    player.x = Clamp(mousePosition, 0, GetScreenWidth() - player.width);
+    float mousePositionWithPlayerOffset = GetMouseX() - player.width / 2;
+    player.x = Clamp(mousePositionWithPlayerOffset, 0, GetScreenWidth() - player.width);
+
+    if (ballPosition.x > GetScreenWidth() - ballRadii || ballPosition.x < 0 + ballRadii)
+      ballSpeed.x *= -1;
+    if (ballPosition.y > GetScreenHeight() - ballRadii || ballPosition.y < 0 + ballRadii)
+      ballSpeed.y *= -1;
+
+    if (CheckCollisionCircleRec(ballPosition, ballRadius, player))
+    {
+      ballSpeed.y *= -1;
+    }
+
+    ballPosition.x += ballSpeed.x * GetFrameTime();
+    ballPosition.y += ballSpeed.y * GetFrameTime();
 
     BeginDrawing();
 
     ClearBackground(BLACK);
 
     DrawRectangleRec(player, WHITE);
+    DrawCircleV(ballPosition, ballRadius, WHITE);
 
     EndDrawing();
   }
